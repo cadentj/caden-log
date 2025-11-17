4fd8ec536ab28c0f5ef862b97f8b61483ca32774
cadentj <caden.juang@gmail.com>
2025-11-17 00:10:04 -0800

    fix env variable

--- apps/web/src/lib/api/queries.ts
+++ apps/web/src/lib/api/queries.ts
@@ -1,9 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { type MessageWithTag, type Tag } from "db";
 
-const API_BASE_URL = import.meta.env.VITE_VERCEL
-  ? import.meta.env.VITE_API_URL
-  : "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export function useMessages() {
   return useQuery<MessageWithTag[]>({
