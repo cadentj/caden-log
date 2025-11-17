fd2c3173b6a89bd2ad87f640d97591f7cde30a93
cadentj <caden.juang@gmail.com>
2025-11-16 20:49:37 -0800

    bump webhook script

--- scripts/setup_webhook.sh
+++ scripts/setup_webhook.sh
@@ -1,8 +1,8 @@
 # deleteWebhook to remove
 
-curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/setWebhook" \
+curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/setupWebhook" \
   -H "Content-Type: application/json" \
   -d '{
-        "url": "https://caden-log-api.vercel.app/api/webhook",
+        "url": "https://caden-log-api.vercel.app/bot/webhook",
         "secret_token": "'${WEBHOOK_TOKEN}'"
       }'
